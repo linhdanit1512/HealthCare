634890a871d6ad7e52673dc84b6d48dcb0e36f4a
lightning001 <wintersoul1212@gmail.com>
2017-05-15 22:55:47 +0700

    hiber

--- src/DAO/DoctorDAO.java
+++ src/DAO/DoctorDAO.java
@@ -9,12 +9,32 @@ import org.hibernate.query.Query;
 import entity.Doctor;
 import util.HibernateUtils;
 
-public class DoctorDAO extends ClassDAO{
+public class DoctorDAO extends ClassDAO {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 2360930720951306835L;
+
+	public static Doctor login(String username, String pass) {
+		Doctor doctor = null;
+		Session session = HibernateUtils.getSessionFactory().getCurrentSession();
+		try {
+			session.getTransaction().begin();
+			String hql = "from " + Doctor.class.getName() + " e where e.username =:name and e.passwords =:pass";
+			Query query = session.createQuery(hql);
+			query.setParameter("name", username);
+			query.setParameter("pass", pass);
+			query.setMaxResults(1);
+			if (query.list().size() > 0)
+				doctor = (Doctor) query.list().get(0);
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		}
+		return doctor;
+	}
 
 	public static List<Doctor> getAllDoctor() {
 		List<Doctor> doctors = new ArrayList<Doctor>();
@@ -31,7 +51,7 @@ public class DoctorDAO extends ClassDAO{
 		}
 		return doctors;
 	}
-	
+
 	public static Doctor getDoctor(int id) {
 		Doctor doctor = null;
 		Session session = HibernateUtils.getSessionFactory().getCurrentSession();
@@ -50,8 +70,8 @@ public class DoctorDAO extends ClassDAO{
 		}
 		return doctor;
 	}
-	
-	public static long getLastID(){
+
+	public static long getLastID() {
 		long result = 0;
 		Session session = HibernateUtils.getSessionFactory().getCurrentSession();
 		try {
---
+++ src/rest/ClinicRest.java
@@ -0,0 +1,30 @@
+package rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import DAO.ClinicDAO;
+
+@Path("/clinic")
+public class ClinicRest {
+
+	@Path("{id}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getClinic(@PathParam("id") String id) {
+		int i = Integer.parseInt(id);
+		return ClinicDAO.getClinic(i).toJson();
+	}
+	
+	@Path("/update/{id}")
+	@PUT
+	@Produces(MediaType.APPLICATION_JSON)
+	public boolean update(@PathParam("id") String id){
+		return false;
+	}
+
+}
