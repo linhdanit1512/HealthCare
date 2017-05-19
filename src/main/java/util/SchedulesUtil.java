0bc1dfe808e51a4df8359ce656ff39d55f26dac3
nguyentuesdd <nguyentued@gmail.com>
2017-05-19 08:30:06 -0700

    Create SchedulesUtil.java

---
+++ src/main/java/util/SchedulesUtil.java
@@ -0,0 +1,15 @@
+package restful.healthCare.util;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import restful.healthCare.entity.Schedules;
+
+public class SchedulesUtil {
+	public static Map<String, List<Schedules>> listAllSchedules = new HashMap<String, List<Schedules>>();
+	/*
+	 * Khởi tạo dữ liệu Schedules ở đây bằng Contructor tương tự như phần
+	 * Doctor
+	 */
+}
