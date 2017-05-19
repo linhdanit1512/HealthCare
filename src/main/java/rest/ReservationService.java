dc8e296acc189119e56acea6a29de91c4b2c9fc5
nguyentuesdd <nguyentued@gmail.com>
2017-05-19 08:32:35 -0700

    Create ReservationService.java

---
+++ src/main/java/rest/ReservationService.java
@@ -0,0 +1,29 @@
+package restful.healthCare.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import restful.healthCare.entity.Reservation;
+import restful.healthCare.util.ReservationUtil;
+
+@Path("/reservation")
+public class ReservationService {
+	@GET
+	@Produces(MediaType.APPLICATION_XML + ";charset=utf-8")
+	public List<List<Reservation>> getReservations() {
+		return new ArrayList<List<Reservation>>(ReservationUtil.listAllReservation.values());
+	}
+	
+	@GET
+	@Path("/{user}")
+	@Produces(MediaType.APPLICATION_XML + ";charset=utf-8")
+	public List<Reservation> getReservationOfDoctor(@PathParam("user") String username) {
+		return ReservationUtil.listAllReservation.get(username);
+	}
+}
