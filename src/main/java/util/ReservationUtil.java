a1dcfa01f5610659bc0576b01e33f8643f195635
nguyentuesdd <nguyentued@gmail.com>
2017-05-19 08:30:38 -0700

    Create ReservationUtil.java

---
+++ src/main/java/util/ReservationUtil.java
@@ -0,0 +1,15 @@
+package restful.healthCare.util;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import restful.healthCare.entity.Reservation;
+
+public class ReservationUtil {
+	public static Map<String, List<Reservation>> listAllReservation = new HashMap<String, List<Reservation>>();
+	/*
+	 * Khởi tạo dữ liệu Reservation ở đây bằng Contructor tương tự như phần
+	 * Doctor
+	 */
+}
