9feb8abc3a03e310e9022b2130fc5c5a652e5103
Nguyen Tu <Nguyen Tu@DESKTOP-Q9Q5HUB>
2017-06-20 10:07:00 +0700

    Sửa lại ca trực trong lập lịch làm việc

--- src/main/java/entity/Shift.java
+++ src/main/java/entity/Shift.java
@@ -32,23 +32,9 @@ public class Shift {
 		case 1:
 			return 7;
 		case 2:
-			return 8;
-		case 3:
-			return 9;
-		case 4:
-			return 10;
-		case 5:
-			return 12;
-		case 6:
 			return 13;
-		case 7:
-			return 14;
-		case 8:
-			return 15;
-		case 9:
-			return 19;
-		case 10:
-			return 20;
+		case 3:
+			return 18;
 		default:
 			return -1;
 		}
@@ -57,25 +43,11 @@ public class Shift {
 	public int getStopTime() {
 		switch (shiftNumber) {
 		case 1:
-			return 8;
-		case 2:
-			return 9;
-		case 3:
-			return 10;
-		case 4:
 			return 11;
-		case 5:
-			return 13;
-		case 6:
-			return 14;
-		case 7:
-			return 15;
-		case 8:
-			return 16;
-		case 9:
-			return 20;
-		case 10:
-			return 21;
+		case 2:
+			return 17;
+		case 3:
+			return 22;
 		default:
 			return -1;
 		}
